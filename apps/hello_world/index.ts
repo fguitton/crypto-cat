6373e9a52679f05e91fd66aa41d144c59e948060
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2023-04-19 16:44:08 +0100

    Add my contract code

--- apps/hello_world/index.ts
+++ apps/hello_world/index.ts
@@ -1,7 +1,7 @@
 import { Router, Notifier } from '@secretarium/trustless-app';
 
 export const say_hello = (arg: ArrayBuffer): void => {
-    Notifier.notify(String.UTF8.encode('Hello, World', true));
+    Notifier.notify(String.UTF8.encode('Hello, World !', true));
 };
 
 export function register_routes(): void {
